730bfad9d173c501348dc7186a45954173b41789
Diponkar Saha <44252172+Diponkar-Saha@users.noreply.github.com>
2024-01-14 16:31:15 +0600

    Add Login + Sign In blank Page

---
+++ src/StackOverflowClone.Web/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StackOverflowClone.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
