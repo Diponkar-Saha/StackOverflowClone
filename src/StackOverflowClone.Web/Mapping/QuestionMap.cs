967bf28e54f56805551b28431b9147f6d767740e
Diponkar-Saha <44252172+Diponkar-Saha@users.noreply.github.com>
2024-01-19 12:33:19 +0600

    Added Mapping

---
+++ src/StackOverflowClone.Web/Mapping/QuestionMap.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate;
+using NHibernate.Mapping.ByCode.Conformist;
+using StackOverflowClone.Web.Entities;
+
+namespace StackOverflowClone.Web.Mapping
+{
+    public class QuestionMap : ClassMapping<Question>
+    {
+        public QuestionMap()
+        {
+            Table("Questions");
+
+            Id(x => x.Id, m => m.Generator(Generators.Identity));
+
+            Property(x => x.UserId);
+            Property(x => x.Title);
+            Property(x => x.Body);
+            Property(x => x.Timestamp);
+        }
+    }
+}
