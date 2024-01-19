2519d25f40d92228f55a82096fe9b56015af06e3
Diponkar-Saha <44252172+Diponkar-Saha@users.noreply.github.com>
2024-01-19 12:33:01 +0600

    Add Entities

---
+++ src/StackOverflowClone.Web/Entities/Answer.cs
@@ -0,0 +1,11 @@
+﻿namespace StackOverflowClone.Web.Entities
+{
+    public class Answer
+    {
+        public virtual int Id { get; set; }
+        public virtual int UserId { get; set; }
+        public virtual int QuestionId { get; set; }
+        public virtual string Body { get; set; }
+        public virtual DateTime Timestamp { get; set; }
+    }
+}
---
+++ src/StackOverflowClone.Web/Entities/Question.cs
@@ -0,0 +1,11 @@
+﻿namespace StackOverflowClone.Web.Entities
+{
+    public class Question
+    {
+        public virtual int Id { get; set; }
+        public virtual int UserId { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Body { get; set; }
+        public virtual DateTime Timestamp { get; set; }
+    }
+}
